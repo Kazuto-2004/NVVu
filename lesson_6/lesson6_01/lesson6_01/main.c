a0a92e3f06c0a57ad921ae735f3d6af09abbfe61
Nguyen Van Vu <nguyenvu20042019@gmail.com>
2022-08-14 22:11:24 +0700

    Khởi tạo project lesson6_01

---
+++ lesson_6/lesson6_01/lesson6_01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lesson6_01
+//
+//  Created by Kazuto on 14/08/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
