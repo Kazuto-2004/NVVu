f68d79dd1e21a81262a6e50be4d6fdc77a1513c5
Nguyen Van Vu <nguyenvu20042019@gmail.com>
2022-08-23 17:24:48 +0700

    btvn6

---
+++ btvn6/btvn6/b1_6.h
@@ -0,0 +1,13 @@
+//
+//  b1_6.h
+//  btvn6
+//
+//  Created by Kazuto on 23/08/2022.
+//
+
+#ifndef b1_6_h
+#define b1_6_h
+
+#include <stdio.h>
+
+#endif /* b1_6_h */
---
+++ btvn6/btvn6/b2_6.h
@@ -0,0 +1,13 @@
+//
+//  b2_6.h
+//  btvn6
+//
+//  Created by Kazuto on 23/08/2022.
+//
+
+#ifndef b2_6_h
+#define b2_6_h
+
+#include <stdio.h>
+
+#endif /* b2_6_h */
