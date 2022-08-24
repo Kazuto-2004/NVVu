7c2c6f5d8a37961acdfb12356c3c97ddb08b44a8
Nguyen Van Vu <nguyenvu20042019@gmail.com>
2022-08-24 16:05:09 +0700

    b1_sualai1

---
+++ bai_thi_qua_mon/bai_thi_qua_mon/b1_sualai1.c
@@ -0,0 +1,8 @@
+//
+//  b1_sualai1.c
+//  bai_thi_qua_mon
+//
+//  Created by Kazuto on 24/08/2022.
+//
+
+#include "b1_sualai1.h"
---
+++ bai_thi_qua_mon/bai_thi_qua_mon/b1_sualai1.h
@@ -0,0 +1,13 @@
+//
+//  b1_sualai1.h
+//  bai_thi_qua_mon
+//
+//  Created by Kazuto on 24/08/2022.
+//
+
+#ifndef b1_sualai1_h
+#define b1_sualai1_h
+
+#include <stdio.h>
+
+#endif /* b1_sualai1_h */
